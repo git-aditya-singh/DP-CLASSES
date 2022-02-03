688d53820a5b3e33c97ebd6b2bcfc56332dc764f
aditya singh <mail2adityasinghsln@gmail.com>
2022-02-04 02:46:21 +0530

    update

--- DAY2/day2.cpp
+++ DAY2/day2.cpp
@@ -163,57 +163,6 @@ using namespace std;
   //tabulation
 
 
-//gfg friends pairing======================================================
-
-  //recursive approach
-  class Solution
-  {
-    public:
-    int mod=(int)1e9+7;
-    int countFriendsPairings(int n) 
-    {  if(n<=2)return n;
-        return (countFriendsPairings(n-1)+countFriendsPairings(n-2)*(n-1))%mod;
-    }
-  };
-
-  //memoization
-  class Solution
-  {
-    public:
-    int mod=(int)1e9+7;
-    
-    long long countFriendsPairings(int n,vector<long long>& dp) 
-    {  if(n<=2)return dp[n]=n;
-       if(dp[n]!=-1)return dp[n];
-       if(n<0)return 0;
-        return dp[n]=(countFriendsPairings(n-1,dp)+(countFriendsPairings(n-2,dp)*(n-1))%mod)%mod;
-    }
-    int countFriendsPairings(int n){
-        vector<long long>dp(n+1,-1);
-         return (int)countFriendsPairings(n,dp);
-    }
-  };
-  
-  //tabulation
-  class Solution
-  {
-    public:
-    int mod=(int)1e9+7;
-    long long countFriendsPairings_(int N,vector<long long>& dp) 
-    {  
-        for(int n=0;n<=N;n++){
-          if(n<=2) {dp[n]= n;continue;}
-          dp[n]=(dp[n-1]+(dp[n-2]*(n-1))%mod)%mod;
-       }
-       return dp[N];
-    }
-    int countFriendsPairings(int n){
-         vector<long long>dp(N+1,-1); 
-         return (int)countFriendsPairings_(n,dp);
-    }
-  };
-
-
 
 
 //main=====================================================================
