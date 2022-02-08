c3cba8341509d3dc53a2e93523507ab030b1524e
aditya singh <mail2adityasinghsln@gmail.com>
2022-02-09 02:48:28 +0530

    update

--- DAY4/day4.cpp
+++ DAY4/day4.cpp
@@ -30,7 +30,34 @@ using namespace std;
   };
   
   //tabulation
-  
+  class Solution {
+    public:
+    int numDistinct(string s, string t) {
+         int n=s.size();
+        int m=t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return helper(s,t,n,m,dp);
+    }
+    int helper(string& s,string& t,int N,int M,vector<vector<int>>& dp){
+    for(int n=0;n<=N;n++){
+        for(int m=0;m<=M;m++){
+          if(m==0){
+                 dp[n][m]=1;continue;
+        }
+        if(n<m){
+             dp[n][m]=0;  continue;  
+        }
+        long long  p=0;
+        if(s[n-1]==t[m-1]){
+            p+= dp[n-1][m-1];
+        }
+        p+= dp[n-1][m];
+         dp[n][m]=p;    
+        }
+    }
+        return dp[N][M];
+    }
+  }; 
 
 
 //main function============================================================================
