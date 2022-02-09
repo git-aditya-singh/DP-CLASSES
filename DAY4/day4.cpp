891264c8bcf0d70b86a07658b1d520f776da0bf9
aditya singh <mail2adityasinghsln@gmail.com>
2022-02-09 19:56:56 +0530

    UPDATE

--- DAY4/day4.cpp
+++ DAY4/day4.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 //leetcode 115 distinct subsequence
+  //recursion
+  class Solution {
+    public:
+    int numDistinct(string s, string t) {
+         int n=s.size();
+        int m=t.size();
+        
+        return helper(s,t,n,m);
+    }
+    int helper(string& s,string& t,int n,int m){
+        if(m==0){
+                return 1;
+        }
+        if(n<m){
+            return 0;    
+        }
+        int p=0;
+        if(s[n-1]==t[m-1]){
+            p+= helper(s,t,n-1,m-1);
+        }
+        p+= helper(s,t,n-1,m);
+        return p;
+    }
+  };
 
   //memoization
   class Solution {
@@ -59,6 +83,71 @@ using namespace std;
     }
   }; 
 
+
+//leetcode 72 edit distance
+
+  //recursion
+  class Solution {
+    public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        return helper(word1,word2,n,m);
+    }
+    int helper(string word1, string word2, int i,int j){
+        if(i==0||j==0)return abs(i-j);
+        
+        if(word1[i-1]==word2[j-1]){
+            return  helper(word1,word2,i-1,j-1);
+        }
+        return (1+min(helper(word1,word2,i-1,j-1),min(helper(word1,word2,i,j-1),helper(word1,word2,i-1,j))));
+    }
+  };
+
+  //memoization
+  class Solution {
+    public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        return helper(word1,word2,n,m,dp);
+    }
+    int helper(string word1, string word2, int i,int j,vector<vector<int>>& dp){
+        if(i==0||j==0)return dp[i][j]= abs(i-j);
+        if(dp[i][j]!=0)return dp[i][j];
+        if(word1[i-1]==word2[j-1]){
+            return dp[i][j]= helper(word1,word2,i-1,j-1,dp);
+        }
+        return dp[i][j]= (1+min(helper(word1,word2,i-1,j-1,dp),min(helper(word1,word2,i,j-1,dp),helper(word1,word2,i-1,j,dp))));
+    }
+  };
+
+  //tabulation
+  class Solution {
+    public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        return helper(word1,word2,n,m,dp);
+    }
+    int helper(string word1, string word2, int I,int J,vector<vector<int>>& dp){
+        for(int i=0;i<=I;i++){
+            for(int j=0;j<=J;j++){
+        if(i==0||j==0){ dp[i][j]= abs(i-j);continue;}
+        
+        if(word1[i-1]==word2[j-1]){
+             dp[i][j]= dp[i-1][j-1];continue;
+        }
+         dp[i][j]= (1+min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j])));
+        }
+    }
+        return dp[I][J];
+    }
+  };
+
+  
 
 //main function============================================================================
 int main(){
