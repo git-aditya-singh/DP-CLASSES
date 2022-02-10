29cce512689ad5a647e2bcff28837a12d059fb0e
aditya singh <mail2adityasinghsln@gmail.com>
2022-02-10 17:38:25 +0530

    UPDATE

--- DAY4/day4.cpp
+++ DAY4/day4.cpp
@@ -147,7 +147,145 @@ using namespace std;
     }
   };
 
+//leetcode 44 wildcard matching
+
+  //recursion
+  class Solution {
+    public:
+    bool isMatch(string s, string p) {
+        p=rem(p);
+        return helper(s,p,s.size(),p.size());
+    }
+    bool helper(string & s,string & p,int n,int m){
+     if(n==0||m==0){
+         if(n==0&&m==0)return 1;
+         if(n == 0 && m==1 &&p[m-1] == '*')
+             return 1;
+         return 0;
+     }
+     if(s[n-1]==p[m-1]||p[m-1]=='?'){
+        return  dp[n][m]=helper(s,p,n-1,m-1);
+     }else if(p[m-1]=='*'){
+         return helper(s,p,n-1,m) || helper(s,p,n,m-1);
+     }else
+     { return 0;
+     }  
+    }
+    string rem(string & str){
+        int n=str.size();
+        if(str.size()==0)return str;
+        int i=1;
+        char ch = (char)(str[0]);
+        string ans;
+        ans += ch;
+        while(i<n){
+            while(i<n&&ans[ans.size()-1]=='*'&&str[i]=='*')i++;
+            if(i<n)
+            {
+                 ans += (char)(str[i]);
+            }   
+            i++;
+        }
+        return ans;  
+    }
+  };
   
+  //memoization
+  class Solution {
+    public:
+    bool isMatch(string s, string p) {
+        p=rem(p);
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return helper(s,p,s.size(),p.size(),dp);
+    }
+    bool helper(string & s,string & p,int n,int m,vector<vector<int>>& dp){
+     if(n==0||m==0){
+         if(n==0&&m==0)return dp[n][m]=1;
+         if(n == 0 && m==1 &&p[m-1] == '*')
+             return dp[n][m]=1;
+         return dp[n][m]=0;
+     }
+     if(dp[n][m]!=-1)return dp[n][m];
+     if(s[n-1]==p[m-1]||p[m-1]=='?'){
+        return  dp[n][m]=helper(s,p,n-1,m-1,dp);
+     }else if(p[m-1]=='*'){
+         return dp[n][m]=helper(s,p,n-1,m,dp) || helper(s,p,n,m-1,dp);
+     }else
+     { return dp[n][m]=0;
+     }  
+    }
+    string rem(string & str){
+        int n=str.size();
+        if(str.size()==0)return str;
+        int i=1;
+        char ch = (char)(str[0]);
+        string ans;
+        ans += ch;
+        while(i<n){
+            while(i<n&&ans[ans.size()-1]=='*'&&str[i]=='*')i++;
+            if(i<n)
+            {
+                 ans += (char)(str[i]);
+            }   
+            i++;
+        }
+        return ans;  
+    }
+  };
+  
+  //tabulation
+  class Solution {
+    public:
+    bool isMatch(string s, string p) {
+        p=rem(p);
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return helper(s,p,s.size(),p.size(),dp);
+    }
+    bool helper(string& s,string& p,int N,int M,vector<vector<int>>& dp){
+       for(int n=0;n<=N;n++)
+           for(int m=0;m<=M;m++){
+            if(n==0||m==0){
+                if(n==0&&m==0){dp[n][m]=1;continue;}
+                if(n == 0 && m==1 &&p[m-1] == '*')
+                {dp[n][m]=1;continue;}
+                else
+                {
+                 dp[n][m]=0;continue;
+                }
+            }
+            if(s[n-1]==p[m-1]||p[m-1]=='?'){
+                dp[n][m]=dp[n-1][m-1];
+            }else if(p[m-1]=='*'){
+                dp[n][m]=(dp[n-1][m] || dp[n][m-1]);
+            }else
+            {  dp[n][m]=0;
+            }  
+        }
+        cout<<dp[N][M];
+        return dp[N][M];
+    } 
+    string rem(string& str){
+        int n=str.size();
+        if(str.size()==0)return str;
+        int i=1;
+        char ch = (char)(str[0]);
+        string ans;
+        ans += ch;
+        while(i<n){
+            while(i<n&&ans[ans.size()-1]=='*'&&str[i]=='*')i++;
+            if(i<n)
+            {
+                 ans += (char)(str[i]);
+            }   
+            i++;
+        }
+        return ans;  
+    }
+  };
+
+
+
+
 
 //main function============================================================================
 int main(){
